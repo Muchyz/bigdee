c3402df19ad5e8c14eef91dfc9881a5489e4d787
Muchyz <Kelvinmuchiri699@gmail.com>
2026-06-05 23:03:10 +0300

    remove resize script

--- resize_og.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-img = Image.open('public/logophoto.png')
-img_resized = img.resize((1200, 630), Image.LANCZOS)
-img_resized.save('public/logophoto.png')
-print(f"✅ Resized to {img_resized.size}")
